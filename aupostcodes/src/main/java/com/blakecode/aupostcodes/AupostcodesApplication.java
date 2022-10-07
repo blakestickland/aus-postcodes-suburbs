8cba41a93b165e746578114fe0c65e41debb8db7
Blake Stickland <blakelists@yahoo.com.au>
2022-10-07 16:44:55 +1100

    added health check controller and application properties boiler plate

---
+++ aupostcodes/src/main/java/com/blakecode/aupostcodes/AupostcodesApplication.java
@@ -0,0 +1,13 @@
+package com.blakecode.aupostcodes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AupostcodesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AupostcodesApplication.class, args);
+	}
+
+}
---
+++ aupostcodes/src/test/java/com/blakecode/aupostcodes/AupostcodesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blakecode.aupostcodes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AupostcodesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
