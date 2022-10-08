a8608da4c9fb4840455519551cf806868c1bac5d
Blake Stickland <blakelists@yahoo.com.au>
2022-10-08 23:32:48 +1100

    can return a list of results based on a suburb search. have to be sure to use whitespace substitutesin query

--- postcodesau/src/main/java/com/blakecode/postcodesau/postcode/PostcodeController.java
+++ postcodesau/src/main/java/com/blakecode/postcodesau/postcode/PostcodeController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,13 +29,28 @@ public class PostcodeController {
 		return postcodeService.getAll();
 	}
 	
-// GET /aupostcodes/{id}
+	// ENDPOINT: GET /aupostcodes/{id}
 	@GetMapping("/{id}")
 	public ResponseEntity<Postcode> getPostcodeById(@PathVariable(value = "id") int id) {
 		Postcode postcode = postcodeService.getPostcode(id)
 				.orElseThrow(() -> new ResourceNotFoundException("Not found with id " + id));
 		
 		return new ResponseEntity<>(postcode, HttpStatus.OK);
+	}
+	
+	// Return search based on suburb entered
+	//  ENDPOINT: GET /aupostcodes/search?suburb={query}
+	// NOTE: query should not have white space. Whitespace can be replaced by an underscore(_) 
+	// or a plus-sign(+) or percentage-sign and the digits 2 and 0 (%20)
+	@GetMapping("/search")
+	public ResponseEntity<List<Postcode>> findBySuburb(@RequestParam("suburb") String query) {
+		List<Postcode> suburbs = postcodeService.findBySuburb(query);
+		
+		if (suburbs.isEmpty()) {
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		}
+		
+		return new ResponseEntity<>(suburbs, HttpStatus.OK);
 	}
 	
 	@PostMapping
--- postcodesau/src/main/java/com/blakecode/postcodesau/postcode/PostcodeRepository.java
+++ postcodesau/src/main/java/com/blakecode/postcodesau/postcode/PostcodeRepository.java
@@ -1,10 +1,17 @@
 package com.blakecode.postcodesau.postcode;
 
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+
 
 public interface PostcodeRepository extends JpaRepository<Postcode, Integer> {
 	@Query (value = "SELECT MAX(id) FROM Postcode")
 	public Integer getMaxId();
+	
+	@Query ("SELECT p FROM Postcode p WHERE " + 
+			"p.suburb LIKE CONCAT('%', :query, '%')")
+	List<Postcode> searchForSuburb(String query);
 }
