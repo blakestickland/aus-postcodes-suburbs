078549e3962da933d396273c0b338d239201325e
Blake Stickland <blakelists@yahoo.com.au>
2022-10-10 11:15:33 +1100

    removed old unused directory

---
+++ postcodesau 2/src/main/java/com/blakecode/postcodesau/PostcodesauApplication.java
@@ -0,0 +1,13 @@
+package com.blakecode.postcodesau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostcodesauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostcodesauApplication.class, args);
+	}
+
+}
